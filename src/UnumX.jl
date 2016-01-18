0b04ce3028775c37b90ab1f72f1ed7f3e6a31ebc
Simon Byrne <simonbyrne@gmail.com>
2016-01-18 10:47:48 +0000

    export Ubound34

--- src/UnumX.jl
+++ src/UnumX.jl
@@ -3,7 +3,7 @@ module UnumX
 import Base: print, show, showcompact, convert, zero, one
 import Base: +, -, *, /, abs, ==, <, <=
 
-export Unum, Unum22, Ubound22, Bnum, Bbound, isexact, esmax, fsmax, emax, fmax
+export Unum, Unum22, Ubound22, Unum34, Ubound34, Bnum, Bbound, isexact, esmax, fsmax, emax, fmax
 
 
 include("interval.jl")
